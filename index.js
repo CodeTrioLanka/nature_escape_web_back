e393fb9f5ed6af6cdaad4df9016d709eae5c433a
ravindu-kandawela1205 <ravindu.l@botcalm.com>
2026-01-25 19:54:57 +0530

    fix vercel

--- index.js
+++ index.js
@@ -3,39 +3,27 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import cors from "cors";
 import homeRoute from "./route/home.route.js";
-import uploadRoute from "./route/upload.route.js";
 import { application } from "./config/application.js";
 import authRoute from './route/auth.route.js';
+
 const app = express();
-const PORT = application.PORT;
 
 // Middleware
-app.use(
-  cors({
-    origin: application.CLIENT_URL,
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: application.CLIENT_URL,
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
-//endpoints
+// Connect to DB
+await connectDB();
 
+// Routes
 app.use("/api/home", homeRoute);
-app.use("/api/upload", uploadRoute);
 app.use('/api/auth', authRoute);
-
-// Health check
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
-try {
-  await connectDB();
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.error("Failed to start server:", error);
-  process.exit(1);
-}
+// Export for Vercel
+export default app;
